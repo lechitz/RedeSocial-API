b3cada667c65c9dcf20e407873f02a23b813866c
Felipe Bravo <felipe.bravo@bemobi.com.br>
2022-09-22 18:42:30 -0300

    Validando o usuário

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //Usuario representa um usuário utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,41 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criado_em,omitempty"`
+}
+
+//Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome) //Removendo os espaços nas extremidades do nome
+	usuario.Nick = strings.TrimSpace(usuario.Nick) //Removendo os espaços nas extremidades do nick
+	usuario.Email = strings.TrimSpace(usuario.Email) //Removendo os espaços nas extremidades do email
 }
