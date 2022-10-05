19ed5bb147dbcb564d75c2de824291fa6519b130
Felipe Bravo <felipe.bravo@bemobi.com.br>
2022-10-05 20:16:09 -0300

    Atualizando um usuário

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -17,8 +17,8 @@ type Usuario struct {
 }
 
 //Preparar vai chamar os métodos para validar e formatar o usuário recebido
-func (usuario *Usuario) Preparar() error {
-	if erro := usuario.validar(); erro != nil {
+func (usuario *Usuario) Preparar(etapa string) error {
+	if erro := usuario.validar(etapa); erro != nil {
 		return erro
 	}
 
@@ -27,7 +27,7 @@ func (usuario *Usuario) Preparar() error {
 	return nil
 }
 
-func (usuario *Usuario) validar() error {
+func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Nome == "" {
 		return errors.New("O nome é obrigatório")
 	}
@@ -40,7 +40,7 @@ func (usuario *Usuario) validar() error {
 		return errors.New("O email é obrigatório")
 	}
 
-	if usuario.Senha == "" {
+	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("A senha é obrigatória")
 	}
 
