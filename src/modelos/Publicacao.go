e8d4576885f3fd4416c6fbcb4fc13f9303bbd9c5
Felipe Bravo <felipe.lechitz@gmail.com>
2022-10-24 21:06:58 -0300

    Validando campos da Publicação

--- src/modelos/Publicacao.go
+++ src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //Publicacao representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,34 @@ type Publicacao struct {
 	AutorNick string `json:"autorNick,omitempty"`
 	Curtidas  uint64 `json:"curtidas"`
 	CriadaEm time.Time `json:"criadaEm,omitempty"`
+}
+
+//Preparar vai chamar os métodos para validar e formatar a publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+
+	return nil
+}
+
+//validar verifica se os campos Titulo e Conteudo possuem conteúdo
+func (publicacao *Publicacao) validar() error{
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório")
+	}
+
+	return nil
+}
+
+//formatar ajusta os espaços do texto do Titulo e Conteudo
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
